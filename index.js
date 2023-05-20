6acbb3ac7f5d9d031200f55ced870351b9079d73
Majedul Hasan <hasanmajedul@gmail.com>
2023-05-20 14:12:05 +0600

    single toy get

--- index.js
+++ index.js
@@ -39,6 +39,14 @@ async function run() {
       const result = await toysCollection.insertOne(toy);
       res.send(result);
     });
+
+    // single toy get
+    app.get('/toys/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toysCollection.findOne(query);
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
